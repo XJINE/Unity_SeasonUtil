2c24cbffde08db39221698f43108d4cbcef0fb01
XJINE <umeda@team-lab.com>
2022-09-15 17:46:35 +0800

    First commit.

---
+++ Assets/Packages/SeasonUtil/SeasonUtil.cs
@@ -0,0 +1,65 @@
+﻿public static class SeasonUtil
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+    public static Season SeasonFromSystemDateMonthInJapaneseMeteorology
+    {
+        // NOTE:
+        // 12,  1,  2 = Winter
+        //  3,  4,  5 = Spring
+        //  6,  7,  8 = Summer
+        //  9, 10, 11 = Autumn
+        // EX:
+        // DateTimeNowMonth = 12 -> 10 = Winter
+        //                  =  3 ->  1 = Spring
+        //                  =  7 ->  5 = Summer
+
+        get
+        {
+            var month = System.DateTime.Now.Month - 2;
+
+            if (month <= 0)
+            {
+                month += 12;
+            }
+
+            return SeasonFromMonthInLunisolarCalendar(month);
+        }
+    }
+
+    public static Season SeasonFromSystemDateMonthInLunisolarCalendar
+        => SeasonFromMonthInLunisolarCalendar(System.DateTime.Now.Month);
+
+    public static Season SeasonFromMonthInLunisolarCalendar(int month)
+    {
+        // NOTE:
+        // Lunisolar-Calendar is a traditional calendar mainly used in Asia like Japan, China or any other.
+
+        switch (month)
+        {
+            case 1:
+            case 2:
+            case 3:
+                return Season.Spring;
+            case 4:
+            case 5:
+            case 6:
+                return Season.Summer;
+            case 7:
+            case 8:
+            case 9:
+                return Season.Autumn;
+            case 10:
+            case 11:
+            case 12:
+            default:
+                return Season.Winter;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Unity_SeasonUtil
+
+Utility to handle season.
+
+## Importing
+
+You can use Package Manager or import it directly.
+
+```
+https://github.com/XJINE/Unity_SeasonUtil.git?path=Assets/Packages/SeasonUtil
+```
+
+## How to Use
+
+There are some properties and functions to get season from month.
+
+```csharp
+public enum Season { Spring, Summer, Autumn, Winter }
+
+public static Season SeasonFromSystemDateMonthInJapaneseMeteorology
+public static Season SeasonFromSystemDateMonthInLunisolarCalendar
+public static Season SeasonFromMonthInLunisolarCalendar(int month)
+```
